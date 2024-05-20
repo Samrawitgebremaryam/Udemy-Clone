17f9283aa657d71ccb6704e383ce5b46d3a84d47
Niyat <niyathannibal@gmail.com>
2024-05-20 21:59:13 +0300

    detailspage

--- Client/src/components/card/Card.jsx
+++ Client/src/components/card/Card.jsx
@@ -5,44 +5,38 @@ import StarIcon from '@mui/icons-material/Star';
 import StarHalfIcon from '@mui/icons-material/StarHalf';
 
 const Card = ({ course }) => {
-	const { title, instructors, rating, image, id, price } = course;
-	const instructorNames = instructors
-		.map((instructor) => instructor.name)
-		.join(', ');
+    const { title, instructors, rating, image, id, price } = course;
+    const instructorNames = instructors.map((instructor) => instructor.name).join(', ');
 
-	const navigate = useNavigate();
+    const navigate = useNavigate();
 
-	const handleClick = () => {
-		navigate(`/Udemy-Clone/courses/${id}`);
-	};
+    const handleClick = () => {
+        navigate(`/Udemy-Clone/courses/${id}`);
+    };
 
-	return (
-		<div className={styles.courseWrapper} onClick={handleClick}>
-			<article className={styles.card}>
-				<figure className={styles.wrapper}>
-					<img
-						src={image}
-						alt=''
-					/>
-				</figure>
-				<section className={styles.body}>
-					<p className={styles.title}>{title}</p>
-					<p className={styles.instructors}>{instructorNames}</p>
-					 <div className='styles.ratingDiv'>
-            <span className='styles.rating'>{rating}</span>
-            <span className='styles.stars'>
-              {[...Array(Math.floor(rating))].map((_, index) => (
-                <StarIcon key={index} />
-              ))}
-              {rating % 1 !== 0 && <StarHalfIcon />}
-            </span>
-            
-          </div>
-					<p className={styles.price}>${price}</p>
-				</section>
-			</article>
-		</div>
-	);
+    return (
+        <div className={styles.courseWrapper} onClick={handleClick}>
+            <article className={styles.card}>
+                <figure className={styles.wrapper}>
+                    <img src={image} alt='' />
+                </figure>
+                <section className={styles.body}>
+                    <p className={styles.title}>{title}</p>
+                    <p className={styles.instructors}>{instructorNames}</p>
+                    <div className={styles.ratingDiv}>
+                        <span className={styles.rating}>{rating}</span>
+                        <span className={styles.stars}>
+                            {[...Array(Math.floor(rating))].map((_, index) => (
+                                <StarIcon key={index} />
+                            ))}
+                            {rating % 1 !== 0 && <StarHalfIcon />}
+                        </span>
+                    </div>
+                    <p className={styles.price}>${price}</p>
+                </section>
+            </article>
+        </div>
+    );
 };
 
 export default Card;
