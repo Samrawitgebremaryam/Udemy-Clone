aad1cbd30b72d7180b4ef6cdf851682e5eb4f5c2
sammy-sasa <151463535+sammysamuti@users.noreply.github.com>
2024-06-12 13:27:19 +0300

    payment-integration-done

---
+++ Server/controller/payment.controller.js
@@ -0,0 +1,18 @@
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
+
+exports.processPayment = async (req, res) => {
+  const { token, amount } = req.body;
+
+  try {
+    const charge = await stripe.charges.create({
+      amount,
+      currency: "usd",
+      source: token,
+      description: "Udemy course payment",
+    });
+
+    res.json({ success: true, charge });
+  } catch (error) {
+    res.status(500).json({ success: false, error: error.message });
+  }
+};
